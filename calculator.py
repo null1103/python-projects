23b3400faaeefd75ca0e749e8a7a869e03d5e925
SK <12840358+null1103@users.noreply.github.com>
2024-06-03 21:41:33 -0700

    Basic calculator

---
+++ calculator.py
@@ -0,0 +1,15 @@
+'''
+This project will implement a basic calculator which performs the four basic operations on provided integers:
+addition, subtraction, multiplication, division
+'''
+
+# Ask user to enter two integers
+
+
+# Ask user which operation they would like to perform
+
+
+# Perform the calculation
+
+
+# Print the result of the calculation
